120a4a97579861eeb6dc98792b712cea1918723c
dotori-0 <sesan131@gmail.com>
2022-07-20 23:04:04 +0900

    [Feat] Set Random Background Color for Each Cell on Each Launch

--- Library/LibraryCollectionViewCell.swift
+++ Library/LibraryCollectionViewCell.swift
@@ -20,14 +20,46 @@ class LibraryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var rateLabel: UILabel!
     
+    enum BackgroundColor {
+        case violet
+        case mint
+        case pink
+        case skyblue
+        case orange
+        
+        var uiColor: UIColor {            
+            switch self {
+                case .violet:
+                    return UIColor(named: "Violet")! 
+                case .mint:
+                    return UIColor(named: "Mint")!
+                case .pink:
+                    return UIColor(named: "Pink")!
+                case .skyblue:
+                    return UIColor(named: "Skyblue")!
+                case .orange:
+                    return UIColor(named: "Orange")!
+            }
+        }
+        
+//        var uiColorArray: [UIColor] {
+//            return [BackgroundColor.violet.uiColor, BackgroundColor.mint.uiColor, BackgroundColor.pink.uiColor, BackgroundColor.skyblue.uiColor, BackgroundColor.orange.uiColor]
+//        }
+    }
+    
+    func getBackgroundColorArray() -> [UIColor] {
+        return [BackgroundColor.violet.uiColor, BackgroundColor.mint.uiColor, BackgroundColor.pink.uiColor, BackgroundColor.skyblue.uiColor, BackgroundColor.orange.uiColor]
+    }
+    
     func configureCell(data: Movie) {
-        #colorLiteral(red: 0.7672912478, green: 0.7759679556, blue: 0.931388557, alpha: 1)
+//        #colorLiteral(red: 0.7672912478, green: 0.7759679556, blue: 0.931388557, alpha: 1)     
+        #colorLiteral(red: 0.7672912478, green: 0.7759679556, blue: 0.931388557, alpha: 1)             
         #colorLiteral(red: 0.6613729596, green: 0.8103576303, blue: 0.7744751573, alpha: 1)
         #colorLiteral(red: 0.9001491666, green: 0.7588487864, blue: 0.7798168659, alpha: 1)
         #colorLiteral(red: 0.7359035611, green: 0.8384718299, blue: 0.86347121, alpha: 1)
         #colorLiteral(red: 0.8850976825, green: 0.7780609727, blue: 0.6756395698, alpha: 1)
 //        containerView.backgroundColor = .systemMint
-        containerView.backgroundColor = #colorLiteral(red: 0.6613729596, green: 0.8103576303, blue: 0.7744751573, alpha: 1)
+        containerView.backgroundColor = getBackgroundColorArray().randomElement()  // force-unwrapping 필요 없는 이유..? 함수에서 이미 확인해줘서?
         containerView.layer.cornerRadius = 20
 //        containerView.clipsToBounds = false
 //        containerView.layer.masksToBounds = true
